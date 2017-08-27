8eaa1c01608fa978f4a7945dd6f85cc76f15fa24
LNeway <453973206@qq.com>
2017-08-27 15:28:38 +0800

    1. glsl 方向变换。

--- app/src/main/res/raw/texture_fragment.glsl
+++ app/src/main/res/raw/texture_fragment.glsl
@@ -6,5 +6,6 @@ uniform sampler2D watermark;
 
 void main()
 {
- gl_FragColor = mix(texture2D(u_samplerTexture, outTexCoord) * vec4(0, 1, 0, 1), texture2D(watermark, outTexCoord), 0.2);
+ gl_FragColor = mix(texture2D(u_samplerTexture, outTexCoord) * vec4(0, 1, 0, 1), texture2D(watermark,
+    vec2(1.0 - outTexCoord.x, outTexCoord.y)), 0.2);
 }
